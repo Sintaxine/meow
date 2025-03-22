9b3816887a9672fdc1889de9a620d74aad7138b3
Sintaxine <karlxguts@gmail.com>
2025-03-22 22:11:22 +0100

    Added Syntax Highlighting >.<

--- meow/meow.py
+++ meow/meow.py
@@ -1,10 +1,20 @@
 import sys
+from pygments import highlight
+from pygments.lexers import get_lexer_for_filename
+from pygments.formatters import TerminalFormatter
 
 def meow(filename):
     try:
         with open(filename, 'r') as f:
-            for line in f:
-                sys.stdout.write(line)
+            code = f.read()
+
+        lexer = get_lexer_for_filename(filename)
+
+        highlighted_code = highlight(code, lexer, TerminalFormatter())
+
+        for line in highlighted_code.splitlines():
+            sys.stdout.write(line + '\n')
+
     except (FileNotFoundError, PermissionError) as e:
         sys.stderr.write(f"Error: {e}\n")
         exit(1)
@@ -14,7 +24,7 @@ def main():
     argc = len(argv) - 1
 
     if argc != 1:
-        sys.stderr.write("Invalid usage: catcopy <filename>\n")
+        sys.stderr.write("Invalid usage: meow <filename>\n")
         exit(1)
 
     meow(argv[1])
